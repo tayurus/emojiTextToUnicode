870d2301b2c33248ca56b6dc22db197f18c616c6
tatarintsev <yuri.tatarintsev@sharp-dev.net>
2019-03-29 10:14:15 +0400

    fix

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const emojiUnicode = require("emoji-unicode"),
   toEmoji = require("emoji-name-map");
 
 // это текст смайлика, который пришел с сервера
-let emojiText = 'grinning';
+let emojiText = 'neutral-face';
+
+//в нем могут быть тире, а emoji устроены так, что в них тире нужно заменить на нижнее подчеркивание
+emojiText = emojiText.replace(/\-/gi, "_");
 
 //преобразуем его в строку Unicode
 let emojiCode = emojiUnicode(toEmoji.get(`:${emojiText}:`));
